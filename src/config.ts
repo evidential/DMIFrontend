7c77cd3c1486237ac4cd96e6b73cffaeb600cbff
Patrick <paddeee@gmail.com>
2024-11-19 11:02:51 +0000

    v1.1.1

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config =  {
-  version: '1.1.0',
+  version: '1.1.1',
   socketServerURL: '//evidentialrealityserver.com',
   s3Bucket: 'https://dmi-frontend.s3.eu-west-2.amazonaws.com/',
   envirinmentModel: 'House.glb',
