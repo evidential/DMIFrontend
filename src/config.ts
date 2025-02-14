b300cb6f34223457e0bd24ddf5a9fdbdf2b19e2f
Patrick <paddeee@gmail.com>
2025-02-14 14:38:20 +0000

    Updated socket url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config =  {
   version: '1.3.0',
-  /*socketServerURL: '//evidentialrealityserver.com',*/
-  socketServerURL: '//192.168.0.31:8080',
+  socketServerURL: '//evidentialrealityserver.com',
+  /*socketServerURL: '//192.168.0.31:8080',*/
   s3Bucket: 'https://dmi-frontend.s3.eu-west-2.amazonaws.com/',
   environmentModel: 'House.glb'
 };
