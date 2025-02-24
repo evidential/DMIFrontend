fecaf65be501efe4a9a0a3d39e3525336517fd1f
Patrick <paddeee@gmail.com>
2025-02-24 12:19:21 +0000

    v1.4.2

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config =  {
-  version: '1.4.0',
+  version: '1.4.2',
   socketServerURL: '//evidentialrealityserver.com',
   /*socketServerURL: '//192.168.0.31:8080',*/
   s3Bucket: 'https://dmi-frontend.s3.eu-west-2.amazonaws.com/',
