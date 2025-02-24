f2db044f73dd3fe7b8ee5e51c9b52e8330eae6d0
Patrick <paddeee@gmail.com>
2025-02-24 16:56:11 +0000

    Added SD Card item

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config =  {
-  version: '1.4.2',
+  version: '1.4.3',
   socketServerURL: '//evidentialrealityserver.com',
-  /*socketServerURL: '//192.168.0.31:8080',*/
+  /*socketServerURL: '//192.168.0.118:8080',*/
   s3Bucket: 'https://dmi-frontend.s3.eu-west-2.amazonaws.com/',
   environmentModel: 'House.glb'
 };
